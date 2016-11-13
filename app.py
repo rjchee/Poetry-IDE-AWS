3bc7d0fc2969ee87ca0055215ace6c110c0b2a0f
Ubuntu <ubuntu@ip-172-31-37-36.us-west-2.compute.internal>
2016-11-13 15:48:25 +0000

    updated app

--- app.py
+++ app.py
@@ -2,29 +2,37 @@ from flask import Flask
 import json
 
 app = Flask(__name__)
-word_dict = json.load(open('dictionary.json', 'r'))
+print("loading json")
+app.config['D'] = json.load(open('dictionary.json', 'r'))
+app.config['W'] = {app.config['D']['list'][i] : i for i in range(len(app.config['D']['list']))}
+print("loaded")
+"""
 app.config['D'] = {
         'words': {
             word_dict['list'][i] : word_dict['words'][i] for i in range(len(word_dict['list']))},
         'rhymes': {word_dict['list'][int(k)]: set(word_dict['list'][idx] for idx in v) for k, v in word_dict['rhymes'].items()}
 }
+"""
+print("made it here")
 
 @app.route('/syllables/<word>', methods=['GET'])
 def get_num_syllables(word=None):
-    return str(app.config['D']['words'][word][0]['c'] if word is not None and word in app.config['D']['words'] else 0)
+    return str(app.config['D']['words'][app.config['W'][word]][0]['c'] if word is not None and word in app.config['W'] else 0)
 
 @app.route('/phonemes/<word>', methods=['GET'])
 def get_phonemes(word=None):
-    return ' '.join(app.config['D']['words'][word][0]['p']) if word is not None and word in app.config['D']['words'] else ''
+    return ' '.join(app.config['D']['words'][app.config['W'][word]][0]['p']) if word is not None and word in app.config['W'] else ''
 
 @app.route('/stresses/<word>', methods=['GET'])
 def get_stresses(word=None):
-    return '' if word is None or word not in app.config['D']['words'] else ''.join(str(int(x)) for x in app.config['D']['words'][word][0]['s'])
+    return '' if word is None or word not in app.config['W'] else ''.join(str(int(x)) for x in app.config['D']['words'][app.config['W'][word]][0]['s'])
 
 @app.route('/rhymes/with/<word_1>/<word_2>/', methods=['GET'])
 def rhymes_with(word_1=None, word_2=None):
-    return str(word_1 is not None and word_2 is not None and word_1 in app.config['D']['rhymes'] and word_2 in app.config['D']['rhymes'][word_2])
+    return str(word_1 is not None and word_2 is not None and word_1 in app.config['W'] and word_2 in app.config['D']['rhymes'][app.config['W'][word_1]])
 
 @app.route('/rhymes/<word>', methods=['GET'])
 def get_rhymes(word=None):
-    return '' if word is None or word not in app.config['D']['rhymes'] else '\n'.join(sorted(app.config['D']['rhymes'][word], key=lambda x:app.config['D']['words'][x][0]['c']))
+    return '' if word is None or word not in app.config['W'] else '\n'.join(sorted((app.config['W'][x] for x in app.config['D']['rhymes'][app.config['W'][word]]), key=lambda x:app.config['D']['words'][x][0]['c']))
+
+app.run()
